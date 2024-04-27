65fb4ae521201707964d9adbeba9b473e2a71366
arielrado <arielrado@gmail.com>
2024-04-27 13:10:24 +0300

    responding to echo with content

--- app/server.c
+++ app/server.c
@@ -6,9 +6,13 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <stdbool.h>
 
-#define HTTP_OK "HTTP/1.1 200 OK\r\n\r\n"
+#define HTTP_OK_EMPTY "HTTP/1.1 200 OK\r\n\r\n"
+#define HTTP_OK "HTTP/1.1 200 OK\r\n"
 #define HTTP_NOT_FOUND "HTTP/1.1 404 NOT FOUND\r\n\r\n"
+#define PLAIN_TEXT "Content-Type: text/plain\r\n"
+#define ECHO_LEN 6
 #define BUFFER_SIZE 1024
 
 typedef struct {
@@ -40,30 +44,50 @@ HttpRequest* parse_request(char* buffer) {
 	return new_request(method, path, version);
 }
 
-int count_tokens(char* str, char* token) {
+int count_tokens(char* str, char token) {
 	int count = 0;
 	char* ptr = str;
 
-	while(ptr!='\0'){
-		if(&ptr == token){
+	while (*ptr != '\0') {
+		if (*ptr == token) {
 			count++;
 		}
 		ptr++;
 	}
+	return count;
 }
 
 char** parse_path(char* path) {
-	int separator_count = count_tokens(path, "/");
-	char* path_components = (char*) malloc(separator_count*sizeof(char));
-	if(path_components==NULL){
+	printf("path: %s\n", path);
+	int separator_count = count_tokens(path, '/');
+	printf("separator count: %d\n", separator_count);
+	char** path_components = (char**)malloc(separator_count * sizeof(char));
+	if (path_components == NULL) {
 		printf("malloc failed!");
 		return NULL;
 	}
-	path_components[0] = strtok(path,"/");
+	path_components[0] = strtok(path, "/");
 	for (int i = 1; i < separator_count; i++) {
 		path_components[i] = strtok(NULL, "/");
 	}
 	return path_components;
+}
+
+bool generate_echo_response(HttpRequest* req, char* buffer) {
+	int i = 0;
+	strcpy(buffer, HTTP_OK);
+	i=strlen(buffer);
+	strcpy(buffer+i, PLAIN_TEXT);
+	i=strlen(buffer);
+	char* string;												// len(/echo/)=6
+	if(0 > asprintf(&string, "Content-Length: %d\r\n\r\n", strlen(req->path+ECHO_LEN))) return false;
+	strcpy(buffer+i, string);
+	i=strlen(buffer);
+	free(string);
+	strcpy(buffer+i, req->path+ECHO_LEN);
+	i=strlen(buffer);
+	strcpy(buffer+i, "\r\n\r\n");
+	return true;
 }
 
 int main() {
@@ -75,6 +99,7 @@ int main() {
 	char buffer[BUFFER_SIZE];
 	struct sockaddr_in client_addr;
 	HttpRequest* request;
+	char** path_components;
 
 	server_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_fd == -1) {
@@ -120,14 +145,22 @@ int main() {
 	printf("recieved request (%d bytes): %s\n", bytes_recieved, buffer);
 
 	request = parse_request(buffer);
+	printf("parsed request: %s %s %s\n", request->method, request->path, request->version);
 	if (request == NULL) {
 		printf("null request!");
 		return 1;
 	}
 
-	if (strcmp(request->path, "/") == 0) {
-		bytes_sent = send(client_fd, HTTP_OK, strlen(HTTP_OK), 0);
-		printf("successfully sent (%d bytes): %s\n", bytes_sent, HTTP_OK);
+	path_components = parse_path(request->path);
+	if (strlen(path_components[0]) == 0) {
+		bytes_sent = send(client_fd, HTTP_OK_EMPTY, strlen(HTTP_OK_EMPTY), 0);
+		printf("successfully sent (%d bytes): %s\n", bytes_sent, HTTP_OK_EMPTY);
+	}
+	else if (strcmp(path_components[0], "echo") == 0) {
+		printf("echo request\n");
+		if(!generate_echo_response(request, buffer)) return 1;
+		bytes_sent = send(client_fd, buffer, strlen(buffer), 0);
+		printf("successfully sent (%d bytes): %s\n", bytes_sent, buffer);
 	}
 	else {
 		bytes_sent = send(client_fd, HTTP_NOT_FOUND, strlen(HTTP_NOT_FOUND), 0);
